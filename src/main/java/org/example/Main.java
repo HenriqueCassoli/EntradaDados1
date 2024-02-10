2b1a437dbe9f123e1e95f5fa59cdf0dd4add6eb6
Cassoli <fernandes.cassoli@gmail.com>
2024-02-10 12:11:08 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner sc = new Scanner(System.in);
+
+        double x;
+        x = sc.nextDouble();
+        System.out.printf("Você digitou : %.2f%n",  x);
+
+        sc.close();
+    }
+}
